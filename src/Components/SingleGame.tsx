ba9ab12714bd083543e8384e53156f836008375d
kieran170 <kieran170@hotmail.co.uk>
2021-08-17 21:25:42 +0100

    severaly hours later i can see get lobby details properly

--- src/Components/SingleGame.tsx
+++ src/Components/SingleGame.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import { games } from '../GamesList';
 import { Link, useParams } from 'react-router-dom';
 import LobbyTable from './LobbyTable';
@@ -14,46 +14,65 @@ interface game {
 }
 
 interface lobbyObj {
-    id: string;
-    lobbyName: string;
+    id?: string;
+    lobbyName?: string;
 }
 
 export default function SingleGame() {
     const userContext = useContext(UserContext)
     let id: string = useParams()
     const [selectedGame, setSelectedGame] = useState<null | game>(null);
+    const [loading, setLoading] = useState(false);
     const [lobbyArray, setLobbyArray] = useState<[] | lobbyObj[]>([]);
     const user = { name: auth.currentUser?.displayName, game: id[0], id: auth.currentUser?.uid };
+
+
+    const getAllLobbies = useCallback(async () => {
+        if (user.name && user.id && id[0]) {
+            const docRef = await firestore.collection(id[0]).get()
+            const db = await firestore.collection(id[0]);
+            const lobbyArr = docRef.docs.map((doc) => {
+                return doc.id
+            })
+            const lobbies = await lobbyArr.map(async (lobbyId) => {
+                let obj: lobbyObj = {};
+                db.doc(lobbyId).collection('Users').get().then((res) => {
+                    const filtered = res.docs.filter(async (doc) => {
+                        return doc.data().lobbyName
+                    })
+                    obj.lobbyName = filtered[0].data().lobbyName;
+                    obj.id = lobbyId
+                })
+                return obj
+            })
+            const newLobs = Promise.all(lobbies)
+            setTimeout(() => {
+                newLobs.then((res) => {
+                    setTimeout(() => {
+                        setLobbyArray(res)
+                        setLoading(true)
+                    }, 1000)
+
+                })
+            })
+
+
+        }
+    },
+        [id, user.id, user.name],
+    );
+
+    useEffect(() => {
+        getAllLobbies()
+    }, [getAllLobbies])
 
 
     useEffect(() => {
         const gameSelected = games.filter(el => el.id === Number(id[0]))
         setSelectedGame(gameSelected[0]);
-        const getAllLobbies = async () => {
-            if (user.name && user.id && id[0]) {
-                const docRef = await firestore.collection(id[0]).get()
-                const db = await firestore.collection(id[0]);
-                const lobbyArr = docRef.docs.map((doc) => {
-                    return doc.id
-                })
-                lobbyArr.forEach((lobbyId) => {
-                    db.doc(lobbyId).collection('Users').get().then((res) => {
-                        const filtered = res.docs.filter((doc) => {
-                            return doc.data().lobbyName
-                        })
-                        const lobbyNameArray = filtered.map((lobby) => {
-                            return { lobbyName: lobby.data().lobbyName, id: lobbyId }
-                        })
-                        setLobbyArray(lobbyNameArray)
-                    })
-                })
-
-            }
-        }
-        getAllLobbies();
     }, [id, user.id, user.name])
 
-    const joinLobby = async (lobbyId: string) => {
+    const joinLobby = async (lobbyId: string | undefined) => {
         if (user.name && user.id && id) {
             const db = await firestore.collection(id[0])
             let check = await (await db.doc(lobbyId).collection('Users').doc(user.id).get()).data();
@@ -80,16 +99,23 @@ export default function SingleGame() {
                         })}>Create lobby</button>
                     </div>
                     <div>
-                        {lobbyArray.map((lobbyID, i) => {
-                            return (
-                                <div key={i} style={{ border: '3px white solid', padding: '30px' }}>
-                                    <p>{lobbyID.lobbyName}</p>
-                                    <Link to={`/lobby/${id[0]}/${lobbyID.id}`}>
-                                        <button onClick={() => joinLobby(lobbyID.id)}>View Lobby</button>
-                                    </Link>
-                                </div>
-                            )
-                        })}
+                        <p>Hi</p>
+                        {loading
+                            ?
+                            <>
+                                {lobbyArray.map((lobby) => {
+                                    return (
+                                        <div key={lobby.id} style={{ border: '3px white solid', padding: '30px' }}>
+                                            <p>{lobby.lobbyName}</p>
+                                            <Link to={`/lobby/${id[0]}/${lobby.id}`}>
+                                                <button onClick={() => joinLobby(lobby.id)}>Join Lobby</button>
+                                            </Link>
+                                        </div>
+                                    )
+                                })}
+                            </>
+                            :
+                            <p>Loading</p>}
                     </div>
                     <div className='single-game-page-table-container'>
                         <LobbyTable />
