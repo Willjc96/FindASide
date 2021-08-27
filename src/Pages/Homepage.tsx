6d2506fa4c6c25ee10ee46f02f4f4de8242d776b
willjc96 <williamjamescook@aol.com>
2021-08-27 15:57:27 +0100

    add more game titles with cover art pictures

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -7,12 +7,20 @@ export default function Homepage() {
 
     const sports = games.filter(el => el.genre === 'Sport');
     const shooter = games.filter(el => el.genre === 'Shooter');
-    
+    const racing = games.filter(el => el.genre === 'Racing');
+    const openWorld = games.filter(el => el.genre === 'OpenWorld');
+    const moba = games.filter(el => el.genre === 'MOBA');
+    const fighter = games.filter(el => el.genre === 'Fighter');
+
     return (
         <div className='page-container'>
             <HomepageGreeting />
             <GamingCategories category='Sports' games={sports} />
             <GamingCategories category='Shooter' games={shooter} />
+            <GamingCategories category='Racing' games={racing} />
+            <GamingCategories category='Open World' games={openWorld} />
+            <GamingCategories category='MOBA' games={moba} />
+            <GamingCategories category='Fighter' games={fighter} />
         </div>
     );
 }
